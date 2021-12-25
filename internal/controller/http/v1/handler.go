22f91bbcfe06c6f4fe4170e026639fe30d1301bc
Pavel <pavel31.pavel31@gmail.com>
2021-12-25 17:08:29 +0200

    routes

--- internal/controller/http/v1/handler.go
+++ internal/controller/http/v1/handler.go
@@ -6,13 +6,9 @@ type Handler struct {
 }
 
 func (h *Handler) Init(api *gin.RouterGroup) {
-	v1 := api.Group("v1")
+	handler := api.Group("v1")
 	{
-		v1.GET("test", func(context *gin.Context) {
-			_, err := context.Writer.Write([]byte("Hello world"))
-			if err != nil {
-				return
-			}
-		})
+		h.initTodoList(handler)
+
 	}
 }
---
+++ internal/controller/http/v1/todolist.go
@@ -0,0 +1,60 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (h *Handler) initTodoList(api *gin.RouterGroup) {
+	todo := api.Group("/todo")
+	{
+		lists := todo.Group("/lists")
+		{
+			lists.GET("/", h.getLists)
+			lists.POST("/", h.createList)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+		}
+		item := todo.Group("/items")
+		{
+			item.GET("/", h.getItems)
+			item.POST("/", h.createItems)
+			item.PUT("/:id", h.updateItems)
+			item.DELETE("/:id", h.deleteItems)
+		}
+	}
+}
+
+func (h *Handler) getLists(c *gin.Context) {
+	c.JSON(http.StatusOK, struct {
+		Test   string `json:"test"`
+		Number int    `json:"number"`
+	}{Test: "First", Number: 1})
+	c.Status(http.StatusOK)
+}
+
+func (h *Handler) updateList(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+func (h *Handler) createList(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+func (h *Handler) deleteList(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+
+func (h *Handler) getItems(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+
+func (h *Handler) createItems(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+
+func (h *Handler) updateItems(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
+
+func (h *Handler) deleteItems(c *gin.Context) {
+	c.Status(http.StatusOK)
+}
